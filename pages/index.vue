5684de31123dbd8120b44d76e3867c029559734d
Matteo Bilotta <me@byloth.net>
2020-11-12 23:31:20 +0100

    [IMP] Added the first implementation of the Card component.

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,13 @@
 <template>
     <div id="home-page" class="container">
         <main>
-            <logo />
+            <card>
+                <logo />
+                <p>Questo è un paragrafo di prova.</p>
+            </card>
             <h1 class="title">
                 Do you dare?
             </h1>
-            <div class="links">
-                <a
-                    href="https://nuxtjs.org/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    class="button--green">
-                    Documentation
-                </a>
-                <a
-                    href="https://github.com/nuxt/nuxt.js"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    class="button--grey">
-                    GitHub
-                </a>
-            </div>
         </main>
     </div>
 </template>
@@ -62,49 +49,10 @@
                 font-weight: 300;
                 letter-spacing: 1px;
             }
-            & > .subtitle
+
+            p
             {
-                color: #526488;
-                font-size: 42px;
-                font-weight: 300;
-                padding-bottom: 15px;
-                word-spacing: 5px;
-            }
-            & > .links
-            {
-                padding-top: 15px;
-
-                & > .button--green
-                {
-                    display: inline-block;
-                    border-radius: 4px;
-                    border: 1px solid #3B8070;
-                    color: #3B8070;
-                    text-decoration: none;
-                    padding: 10px 30px;
-
-                    &:hover
-                    {
-                        color: #fff;
-                        background-color: #3B8070;
-                    }
-                }
-                & > .button--grey
-                {
-                    display: inline-block;
-                    border-radius: 4px;
-                    border: 1px solid #35495E;
-                    color: #35495E;
-                    text-decoration: none;
-                    padding: 10px 30px;
-                    margin-left: 15px;
-
-                    &:hover
-                    {
-                        color: #fff;
-                        background-color: #35495E;
-                    }
-                }
+                margin: 1em 0px;
             }
         }
     }
